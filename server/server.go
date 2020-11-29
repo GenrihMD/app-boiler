6f1a0a776e83828a9fa0a5b67378d3a9d934bbd1
laamin <genrih.md@gmail.com>
2020-11-29 09:00:25 +0300

    Add playground

--- server/server.go
+++ server/server.go
@@ -8,7 +8,7 @@ import (
 	"os"
 
 	"github.com/99designs/gqlgen/graphql/handler"
-	// "github.com/99designs/gqlgen/graphql/playground"
+	"github.com/99designs/gqlgen/graphql/playground"
 )
 
 const defaultPort = "8081"
@@ -21,8 +21,8 @@ func main() {
 
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graph.Resolver{}}))
 
-	// http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/", srv)
+	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
+	http.Handle("/query", srv)
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
