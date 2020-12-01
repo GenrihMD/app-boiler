36e293668109802038ce5f425c4efbee404aa790
laamin <genrih.md@gmail.com>
2020-12-01 19:04:26 +0300

    Add primus

--- client/src/main.js
+++ client/src/main.js
@@ -1,8 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Primus from 'primus'
+import store from './store'
 
 Vue.config.productionTip = false
 
+Vue.use(Primus, {
+  Global: { defaultDesign: 'vuetify' },
+  API: {
+    axiosConnector: {one: 1},
+    apolloConnector: {two: 2},
+    defaultConnector: 'apollo'
+  }
+})
+
+
 new Vue({
-  render: h => h(App),
+  render: h => h(App), 
+  store
 }).$mount('#app')
